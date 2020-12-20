0c9a8b31a35ebd4982ce581e1cdde2185adfbe0f
Retanar <59249123+retanar@users.noreply.github.com>
2020-12-20 11:09:31 +0200

    0.2.0 version. Divergence can now be viewed and changed from main activity. Fixed bug when the widget won't start on the first launch. Some refactoring was made.

--- app/src/main/java/com/vlprojects/divergence/DivergenceWidget.kt
+++ app/src/main/java/com/vlprojects/divergence/DivergenceWidget.kt
@@ -2,24 +2,34 @@ package com.vlprojects.divergence
 
 import android.appwidget.AppWidgetManager
 import android.content.Context
+import android.util.Log
 import android.widget.RemoteViews
 import kotlin.math.absoluteValue
 import kotlin.random.Random
 
-private const val SHARED_FILENAME = "divergence_widget"
-private const val SHARED_DIVERGENCE = "divergence_value"
-private const val ALPHA_WORLDLINE = 0
-private const val BETA_WORLDLINE = 1_000_000
-private const val OMEGA_WORLDLINE = -1_000_000
+const val SHARED_FILENAME = "divergence_widget"
+const val SHARED_DIVERGENCE = "divergence_value"
+
+// TODO: change worldlines to ranges
+const val ALPHA_WORLDLINE = 0
+const val BETA_WORLDLINE = 1_000_000
+const val OMEGA_WORLDLINE = -1_000_000
+
 private const val DIVERGENCE_CHANGE_STEP = 100_000
 private const val MAX_DIVERGENCE_COEFICIENT = DIVERGENCE_CHANGE_STEP / 4
 
+// TODO (DONE): make the widget work from the first launch
 class DivergenceWidget : android.appwidget.AppWidgetProvider() {
 
     // first launch action
-    // TODO: make it work from the first launch
     override fun onEnabled(context: Context?) {
         val prefs = context!!.getSharedPreferences(SHARED_FILENAME, 0)
+        val currentDiv = prefs.getInt(SHARED_DIVERGENCE, Int.MIN_VALUE)
+        Log.d("DivergenceWidget", "onEnabled() call. Current divergence = $currentDiv")
+
+        if (currentDiv >= OMEGA_WORLDLINE)
+            return
+
         with(prefs.edit()) {
             putInt(
                 SHARED_DIVERGENCE,
@@ -27,6 +37,8 @@ class DivergenceWidget : android.appwidget.AppWidgetProvider() {
             )
             apply()
         }
+
+        super.onEnabled(context)
     }
 
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
@@ -35,20 +47,19 @@ class DivergenceWidget : android.appwidget.AppWidgetProvider() {
         val newDiv = generateRandomDivergence(
             prefs.getInt(
                 SHARED_DIVERGENCE,
-                // Shitcode: initial divergence generation
-                OMEGA_WORLDLINE
+                ALPHA_WORLDLINE
             )
         )
         val newDivDigits = splitIntegerToDigits(newDiv)
 
-        appWidgetIds.forEach {
-            updateAppWidget(context, appWidgetManager, it, newDivDigits)
-        }
+        appWidgetIds.forEach { updateAppWidget(context, appWidgetManager, it, newDivDigits) }
 
         with(prefs.edit()) {
             putInt(SHARED_DIVERGENCE, newDiv)
             apply()
         }
+
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
     }
 
     fun updateAppWidget(
@@ -84,37 +95,49 @@ class DivergenceWidget : android.appwidget.AppWidgetProvider() {
     fun generateRandomDivergence(currentDiv: Int): Int {
         /* Coefficient needed to lower the chance of going to new wordline
          * How it works:
-         *  - equalize divergence to range [0;1_000_000)
-         *  - subtract half the maximum divergence to put in range [-500_000;+500_000)
-         *  - divide by specific number to create adding coefficient of half the step */
-        val coef = (when {
-            currentDiv >= BETA_WORLDLINE -> -BETA_WORLDLINE
-            currentDiv < ALPHA_WORLDLINE -> +BETA_WORLDLINE
-            else -> 0
-        } + currentDiv - 500_000) / -(BETA_WORLDLINE / 2 / MAX_DIVERGENCE_COEFICIENT)
+         *  - equalize the divergence to range [0;1_000_000)
+         *  - subtract half of the maximum divergence to put the divergence in range [-500_000;+500_000)
+         *  - divide by a specific number to create the coefficient */
+        val coef = getCoefficient(currentDiv)
 
         var newDiv = currentDiv + Random.nextInt(-DIVERGENCE_CHANGE_STEP + coef, DIVERGENCE_CHANGE_STEP + coef)
-        // println("$currentDiv + (${coef - DIVERGENCE_CHANGE_STEP} ; ${coef + DIVERGENCE_CHANGE_STEP})")
 
-        while (newDiv < OMEGA_WORLDLINE || newDiv > 2_000_000)
+        Log.d(
+            "DivergenceWidget", """generateRandomDivergence() call
+                                    |Previous div: $currentDiv 
+                                    |Step limits: (${-DIVERGENCE_CHANGE_STEP + coef} ; ${DIVERGENCE_CHANGE_STEP + coef})
+                                    |Step: ${newDiv - currentDiv}
+                                    |New div: $newDiv""".trimMargin()
+        )
+
+        while (newDiv < OMEGA_WORLDLINE || newDiv > 2_000_000) {
             if (newDiv < OMEGA_WORLDLINE)
                 newDiv += Random.nextInt(DIVERGENCE_CHANGE_STEP)
             else if (newDiv > 2_000_000)
                 newDiv -= Random.nextInt(DIVERGENCE_CHANGE_STEP)
+        }
 
         return newDiv
     }
 
-    // Shitcode: Unfixable
-    fun splitIntegerToDigits(int: Int): IntArray {
+    private fun getCoefficient(currentDiv: Int): Int {
+        return (when {
+            currentDiv >= BETA_WORLDLINE -> -BETA_WORLDLINE
+            currentDiv < ALPHA_WORLDLINE -> +BETA_WORLDLINE
+            else -> 0
+        } + currentDiv - 500_000) / -(BETA_WORLDLINE / 2 / MAX_DIVERGENCE_COEFICIENT)
+    }
+
+    // Shitcode
+    fun splitIntegerToDigits(number: Int): IntArray {
         val digits = IntArray(7)
-        var integer = int.absoluteValue
+        var integer = number.absoluteValue
 
         for (i in digits.indices) {
             digits[i] = (integer % 10)
             integer /= 10
         }
-        if (int <= 0)
+        if (number <= 0)
             digits[6] = -1
 
         return digits
--- app/src/main/java/com/vlprojects/divergence/MainActivity.kt
+++ app/src/main/java/com/vlprojects/divergence/MainActivity.kt
@@ -2,6 +2,9 @@ package com.vlprojects.divergence
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +12,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // TODO: show divergence in app
+        // TODO (somewhat done): show divergence in app
+        val prefs = getSharedPreferences(SHARED_FILENAME, 0)
+        divergenceText.text = prefs.getInt(SHARED_DIVERGENCE, Int.MIN_VALUE).toString()
+
+        changeDivergenceButton.setOnClickListener {
+            val userDiv = userDivergence.text.toString()
+            val userDivNumber = (userDiv.toDouble() * 1_000_000).roundToInt()
+
+            if (userDiv.isBlank())
+                return@setOnClickListener
+
+            if (userDivNumber !in OMEGA_WORLDLINE..(2 * BETA_WORLDLINE)) {
+                Toast.makeText(this, "Wrong value", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            with(prefs.edit()) {
+                putInt(SHARED_DIVERGENCE, userDivNumber)
+                apply()
+            }
+        }
     }
 }
