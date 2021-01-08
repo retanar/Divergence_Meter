d329c26a7e4739067e12afa652522822c79f9330
Retanar <59249123+retanar@users.noreply.github.com>
2021-01-08 15:03:03 +0200

    Deletion of unused resources, small design improvements and fixes

--- app/src/test/java/com/vlprojects/divergence/ExampleUnitTest.kt
+++ app/src/test/java/com/vlprojects/divergence/ExampleUnitTest.kt
@@ -12,17 +12,4 @@ class ExampleUnitTest {
         println("omega range: $OMEGA_RANGE")
         println("all range: $ALL_RANGE")
     }
-
-    // @Test
-    fun testRandomizer() {
-        val dw = DivergenceWidget()
-        var newDiv = 500_000
-        var iteration = 0
-        while (newDiv in 0..999999) {
-            newDiv = dw.generateBalancedRandomDivergence(newDiv)
-            iteration++
-            //print("$newDiv, ")
-        }
-        println("$newDiv on iteration $iteration")
-    }
 }
