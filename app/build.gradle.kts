51f37113efc5c70ff3cc6651188e47b8a519fe6c
Retanar <59249123+retanar@users.noreply.github.com>
2021-04-13 21:28:28 +0300

    Migrate Gradle to Kotlin DSL

---
+++ app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+android {
+    compileSdkVersion(30)
+    buildToolsVersion("29.0.2")
+    defaultConfig {
+        minSdkVersion(16)
+        targetSdkVersion(30)
+        versionCode = 1
+        versionName = "0.5.0 beta 1"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        resConfigs("en")
+    }
+    buildTypes {
+        getByName("release") {
+            isShrinkResources = true
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("include" to listOf("*.jar"), "dir" to "libs")))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.32")
+    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("androidx.preference:preference:1.1.1")
+    implementation("com.jakewharton.timber:timber:4.7.1")
+    testImplementation("junit:junit:4.13")
+}
