ed21f240a7dae7b779cebbbde7c27ad41e0990a4
Retanar <59249123+retanar@users.noreply.github.com>
2021-04-21 21:33:25 +0300

    Fixed EditText in settings and changed behaviour when no widgets are added

--- app/src/main/java/com/vlprojects/divergence/logic/Constants.kt
+++ app/src/main/java/com/vlprojects/divergence/logic/Constants.kt
@@ -8,7 +8,7 @@ const val SHARED_NEXT_DIVERGENCE = "divergence_next_value"
 const val SHARED_LAST_ATTRACTOR_CHANGE = "last_attractor_change_time"
 
 const val SETTING_ATTRACTOR_NOTIFICATIONS = "attractor_notifications"
-const val SETTING_WORLDLINE_NOTOFICATIONS = "known_worldline_notifications"
+const val SETTING_WORLDLINE_NOTIFICATIONS = "known_worldline_notifications"
 const val SETTING_ATTRACTOR_COOLDOWN_HOURS = "attractor_change_cooldown"
 
 const val CHANGE_WORLDLINE_NOTIFICATION_CHANNEL = "change_worldline_channel"
@@ -18,28 +18,24 @@ const val MILLION = 1_000_000
 const val UNDEFINED_DIVERGENCE = Int.MIN_VALUE
 const val ATTRACTOR_DEFAULT_COOLDOWN_MS = 86_400_000L        // 1 day
 
-val nixieNumberDrawables by lazy {
-    arrayOf(
-        R.drawable.nixie0,
-        R.drawable.nixie1,
-        R.drawable.nixie2,
-        R.drawable.nixie3,
-        R.drawable.nixie4,
-        R.drawable.nixie5,
-        R.drawable.nixie6,
-        R.drawable.nixie7,
-        R.drawable.nixie8,
-        R.drawable.nixie9
-    )
-}
-val tubeIds by lazy {
-    arrayOf(
-        R.id.tube0,
-        R.id.tube1,
-        R.id.tube2,
-        R.id.tube3,
-        R.id.tube4,
-        R.id.tube5,
-        R.id.tube6
-    )
-}
+val nixieNumberDrawables = arrayOf(
+    R.drawable.nixie0,
+    R.drawable.nixie1,
+    R.drawable.nixie2,
+    R.drawable.nixie3,
+    R.drawable.nixie4,
+    R.drawable.nixie5,
+    R.drawable.nixie6,
+    R.drawable.nixie7,
+    R.drawable.nixie8,
+    R.drawable.nixie9
+)
+val tubeIds = arrayOf(
+    R.id.tube0,
+    R.id.tube1,
+    R.id.tube2,
+    R.id.tube3,
+    R.id.tube4,
+    R.id.tube5,
+    R.id.tube6
+)
