b3a800a0acc311d25ade292f1d10179f8458f3a7
Retanar <retanarrekotars@gmail.com>
2026-05-13 19:47:46 +0300

    It shows current divergence on opening

--- app/src/main/java/retanar/divergence/FullscreenMeterActivity.kt
+++ app/src/main/java/retanar/divergence/FullscreenMeterActivity.kt
@@ -1,14 +1,34 @@
 package retanar.divergence
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import retanar.divergence.databinding.ActivityFullscreenMeterBinding
+import retanar.divergence.logic.DivergenceMeter
+import retanar.divergence.util.DI
 
 class FullscreenMeterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFullscreenMeterBinding
+
+    private lateinit var tubes: Array<ImageView>
+    private val tubeStates = arrayOf(0, 0, 0, 0, 0, 0, 0, 0)
+    private val tubeImages = arrayOf(
+        R.drawable.nixie0,
+        R.drawable.nixie1,
+        R.drawable.nixie2,
+        R.drawable.nixie3,
+        R.drawable.nixie4,
+        R.drawable.nixie5,
+        R.drawable.nixie6,
+        R.drawable.nixie7,
+        R.drawable.nixie8,
+        R.drawable.nixie9,
+        R.drawable.nixie_minus,
+        R.drawable.nixie_dot,
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +36,34 @@ class FullscreenMeterActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         hideBars()
+        setupViews()
+    }
+
+    private fun setupViews() {
+        tubes = arrayOf(
+            binding.tube0,
+            binding.tube1,
+            binding.tube2,
+            binding.tube3,
+            binding.tube4,
+            binding.tube5,
+            binding.tube6,
+            binding.tube7,
+        )
+
+        // Put current divergence as a start value
+        val div = DI.preferences.getDivergenceOrCreate()
+        val digits = DivergenceMeter.splitIntegerToDigits(div.intValue)
+        for (i in 0..<(tubes.size - 2)) {
+            setTube(i, digits[i])
+        }
+        setTube(tubes.size - 2, tubeImages.indexOf(R.drawable.nixie_dot))
+        setTube(tubes.size - 1, digits.last())
+    }
+
+    private fun setTube(index: Int, state: Int) {
+        tubeStates[index] = state
+        tubes[index].setImageResource(tubeImages[state])
     }
 
     private fun hideBars() {
